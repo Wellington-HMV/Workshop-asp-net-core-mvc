540354ab7cb163a5ba10230fbca210481d781a18
Wellington <well.raiser@gmail.com>
2019-08-01 23:42:51 -0300

    first commit

---
+++ sistem-sales-and-shopping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sistem_sales_and_shopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
