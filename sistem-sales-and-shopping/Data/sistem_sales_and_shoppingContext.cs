5d58eab6cf0fc2b7c80eaec6feef404fc2d3ffb7
Wellington <well.raiser@gmail.com>
2019-08-04 05:59:56 -0300

    CRUD Scaffolding

---
+++ sistem-sales-and-shopping/Data/sistem_sales_and_shoppingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace sistem_sales_and_shopping.Models
+{
+    public class sistem_sales_and_shoppingContext : DbContext
+    {
+        public sistem_sales_and_shoppingContext (DbContextOptions<sistem_sales_and_shoppingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<sistem_sales_and_shopping.Models.Department> Department { get; set; }
+    }
+}
--- sistem-sales-and-shopping/Startup.cs
+++ sistem-sales-and-shopping/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using sistem_sales_and_shopping.Models;
 
 namespace sistem_sales_and_shopping
 {
@@ -33,6 +35,9 @@ namespace sistem_sales_and_shopping
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<sistem_sales_and_shoppingContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("sistem_sales_and_shoppingContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
