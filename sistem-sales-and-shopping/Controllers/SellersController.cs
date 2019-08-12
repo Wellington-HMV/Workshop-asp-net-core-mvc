d1ba0b297e533de84f119aa2445a51a15db48569
Wellington Vitorino <wellington.vitorino@onlineapp.com.br>
2019-08-12 15:00:50 -0300

    Returning custon error page

--- sistem-sales-and-shopping/Controllers/SellersController.cs
+++ sistem-sales-and-shopping/Controllers/SellersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using system_sales_and_shopping.Models;
 using system_sales_and_shopping.Models.ViewModels;
 using system_sales_and_shopping.Services.Exception;
+using System.Diagnostics;
 
 namespace system_sales_and_shopping.Controllers
 {
@@ -41,12 +42,12 @@ namespace system_sales_and_shopping.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not Provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -61,12 +62,12 @@ namespace system_sales_and_shopping.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -74,12 +75,12 @@ namespace system_sales_and_shopping.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             List<Department> departmens = _departmentService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departmens };
@@ -98,13 +99,19 @@ namespace system_sales_and_shopping.Controllers
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch(DbConcurrencyException){
-                return BadRequest();
-            }
+        }
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        };
+        return View(viewModel);
         }
     }
 }
--- sistem-sales-and-shopping/Models/ViewModels/ErrorViewModel.cs
+++ sistem-sales-and-shopping/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace system_sales_and_shopping.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
