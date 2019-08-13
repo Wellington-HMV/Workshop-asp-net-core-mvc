b6018624df60af8c5c068c1340a9801e68ad2c7a
Wellington Vitorino <wellington.vitorino@onlineapp.com.br>
2019-08-13 13:45:22 -0300

    Preparing sales search navigation views

---
+++ sistem-sales-and-shopping/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace system_sales_and_shopping.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSeach()
+        {
+            return View();
+        }
+    }
+}
